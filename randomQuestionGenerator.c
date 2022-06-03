134e1af38c73280f3b63f7afc3ddbaed05fa6eab
SivaPullaiah <mangalisivapullaiah@gmail.com>
2022-06-03 19:08:35 +0530

    some new line character for showing data clean

--- randomQuestionGenerator.c
+++ randomQuestionGenerator.c
@@ -1,5 +1,16 @@
 /*For generating ramdom questions from given question
 by the help of question number*/
+            // case 1: printf("\n\n"); break;
+            // case 2: printf("\n\n"); break;
+            // case 3: printf("\n\n"); break;
+            // case 4: printf("\n\n"); break;
+            // case 5: printf("\n\n"); break;
+            // case 6: printf("\n\n"); break;
+            // case 7: printf("\n\n"); break;
+            // case 8: printf("\n\n"); break;
+            // case 9: printf("\n\n"); break;
+            // case 10: printf("\n\n"); break;
+            // case 11: printf("\n\n"); break;
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
